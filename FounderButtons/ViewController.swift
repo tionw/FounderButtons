820bf8d8c088d73f4f6cf32738b1852811583d12
user150978 <user150978@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-23 18:00:14 -0500

    Created FounderButtons app

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func googleButtonPressed(_ sender: Any) {
+        topLabel.text = "Larry Page"
+        bottomLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func rtrButtonPressed(_ sender: Any) {
+        topLabel.text = "Jennifer Hyman"
+        bottomLabel.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        topLabel.text = ""
+        bottomLabel.text = ""
+    }
 }
 
